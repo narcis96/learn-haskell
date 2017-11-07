5c1cd66eaf8bcd33e98e9ab1c8323f064413c196
Narcis Gemene <ngemene@ixiacom.com>
2017-11-07 18:53:19 +0200

    add list operations

--- helloworld/helloworld.hs
+++ helloworld/helloworld.hs
@@ -1,7 +1,29 @@
 import System.Environment
+import Test.QuickCheck
 
 mySum ::Int -> Int -> Int
 mySum a b = a+b
+
+square ::Int -> Int
+square x = x * x
+
+double ::Int -> Int
+double x = x + x 
+
+isTriple ::Int -> Int -> Int -> Bool
+isTriple a b c = (square(a) + square(b) == square(c))
+
+param1 ::Int -> Int -> Int
+param1 x y = square x - square y
+
+param2 ::Int -> Int -> Int
+param2 x y = double (x*y)
+
+param3 ::Int -> Int -> Int
+param3 x y = mySum (square x) (square y)
+
+prop_triple ::Int -> Int -> Bool
+prop_triple x y = isTriple (param1 x y) (param2 x y) (param3 x y)
 
 fib ::Int -> Int
 fib 0 = 1
@@ -18,9 +40,10 @@ main = do
 	first <- getLine
 	second <- getLine
 	let x = read first :: Int
-	let y = read second :: Int
-	putStrLn (first ++ " + " ++ second ++ " = " ++ show(mySum x y))
-	putStrLn (head args)
+	putStrLn (first ++ " + " ++ second ++ " = " ++ show (mySum x (read second::Int)))
+	quickCheck prop_triple	
+--	putStrLn (double ("boss"))
+--	putStrLn (head args)
 --let var = read (head args) :: String
 	--putStrLn (head args :: Strin)
 --putStrLn(args)
--- lists/lists.hs
+++ lists/lists.hs
@@ -1,4 +1,19 @@
 
+mySum :: [Int] -> Int
+mySum [] = 0
+mySum a = head a + mySum (tail a)
+
+average x y = (x + y)/2
+
+myMax [] = 0
+myMax (x:tail) = max x (myMax tail)
+
+multiplyBy ::[Int] -> Int -> [Int]
+multiplyBy a b = [x*b| x<- a]
+
+
 main = do
 	let myList = [x^2| x <- [1..10], x `mod` 2 == 0]
 	print myList
+	print (multiplyBy myList 3)
+	print (mySum myList)
